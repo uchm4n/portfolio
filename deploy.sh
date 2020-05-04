91b56506a8a3f7456cafe604a19d74905c8efe79
UchMan <ucha@clips4sale.com>
2020-05-04 03:35:29 -0400

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,4 @@
 # abort on errors
 set -e
 
-# navigate into the build output directory
-cd docs/
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-if [ -d ".git/" ]; then
-   sudo rm .git/ -r
-fi
-
-git init
-git remote add origin git@github.com:uC137/portfolio.git
-git add -A
-git fetch
-git checkout vuepress
-git commit -m 'deploy'
-git push -f git@github.com:uC137/portfolio.git vuepress:gh-pages
-
-cd -
+git push origin `git subtree split --prefix docs master`:gh-pages --force
