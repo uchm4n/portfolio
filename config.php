3e211b382794843bd8aef82d09634315fa80b9ff
UchMan <ucha@clips4sale.com>
2022-06-15 02:22:21 +0400

    baseUrl fix

--- config.php
+++ config.php
@@ -3,40 +3,44 @@
 use Illuminate\Support\Str;
 
 return [
-    'baseUrl' => '',
-    'production' => false,
-    'siteName' => 'Full Stack Developer',
-    'siteDescription' => 'Ucha Chilachava',
+    'baseUrl'             => '',
+    'production'          => false,
+    'siteName'            => 'Full Stack Developer',
+    'siteDescription'     => 'Ucha Chilachava',
     'google_analytics_id' => 'UA-59229127-1',
 
     // Algolia DocSearch credentials
-    'docsearchApiKey' => env('DOCSEARCH_KEY'),
-    'docsearchIndexName' => env('DOCSEARCH_INDEX'),
+    'docsearchApiKey'     => env('DOCSEARCH_KEY'),
+    'docsearchIndexName'  => env('DOCSEARCH_INDEX'),
 
     // navigation menu
-    'navigation' => require_once('navigation.php'),
+    'navigation'          => require_once('navigation.php'),
 
     // helpers
-    'isActive' => function ($page, $path) {
+    'isActive'            => function ($page, $path) {
         return Str::endsWith(trimPath($page->getPath()), trimPath($path));
     },
-    'isActiveParent' => function ($page, $menuItem) {
+    'isActiveParent'      => function ($page, $menuItem) {
         if (is_object($menuItem) && $menuItem->children) {
             return $menuItem->children->contains(function ($child) use ($page) {
                 return trimPath($page->getPath()) == trimPath($child);
             });
         }
     },
+
     'url' => function ($page, $path) {
-        return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
+        return Str::startsWith($path, 'http') ? $path : '/'.trimPath($path);
     },
 
-    'nav' => function($page){
-        return $page->navigation->map(function($item){
-            if (!empty($page->baseUrl)){
-                $item->url = $page->baseUrl . $item->url;
-            }
-            return $item;
+    'nav' => function ($page) {
+        return $page->navigation->map(function ($item, $label) use ($page) {
+            return $item->map(function ($i) use ($page) {
+                $b = $page->baseUrl;
+                if (!empty($b)) {
+                    $i = rtrim($b, '/').$i;
+                }
+                return $i;
+            });
         });
     }
 
--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -42,7 +42,7 @@
         <header class="flex items-center shadow bg-white border-b h-24 mb-8 py-4" role="banner">
             <div class="container flex items-center max-w-8xl mx-auto px-4 lg:px-8">
                 <div class="flex items-center">
-                    <a href="{{ $page->baseUrl }}/" title="{{ $page->siteName }} home" class="inline-flex items-center">
+                    <a href="@if(!empty($page->baseUrl)) {{$page->baseUrl}} @else {{'/'}} @endif" title="{{ $page->siteName }} home" class="inline-flex items-center">
                         <img class="h-8 md:h-10 mr-3" src="{{ $page->baseUrl }}/assets/img/icon-terminal.svg" alt="{{ $page->siteName }} logo" />
                     </a>
                 </div>
