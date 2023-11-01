4b8d04e5a6f0b7cfea89305560de11ef0e5cc32c
UchMan <ucha@clips4sale.com>
2023-11-02 03:06:16 +0400

    updates link

--- source/index.blade.php
+++ source/index.blade.php
@@ -48,7 +48,7 @@
                     <a href="{{ $page->baseUrl }}/pages/education" title="Details" class="bg-gray-600 hover:bg-blue-800 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">
                         Details
                     </a>
-                    <a href="https://drive.google.com/u/0/uc?id=1qT7aFm--TW-QKvc7GDHcpArnzYz-jxQ9&export=download" title="Details" target="_blank" class="bg-blue-600 hover:bg-blue-800 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">
+                    <a href="https://drive.google.com/u/0/uc?id=1BG9ZkbtY11YMVqQ2j3pfVg43VK06z3vC&export=download" title="Details" target="_blank" class="bg-blue-600 hover:bg-blue-800 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">
                         Download
                     </a>
                 </div>
